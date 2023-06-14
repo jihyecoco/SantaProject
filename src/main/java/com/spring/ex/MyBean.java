5d0efe5cba6fa838b670bf54ef9a81c70c2172b9
Gxofl <xofl402@gmail.com>
2023-06-14 09:36:58 +0900

    [가영] 23/06/14 : MyBean 수정

--- src/main/java/com/spring/ex/MyBean.java
+++ src/main/java/com/spring/ex/MyBean.java
@@ -10,26 +10,37 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MyBean implements InitializingBean {
-	
+
 	@Autowired
 	ServletContext servletContext;
-	
+
 	@PostConstruct
-	public void init() {
+	public void init() { 
 		System.out.println("------init()------");
 	}
-	
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		System.out.println("-----afterPropertiesSet()-----");
-		
+
 		String uploadPath = servletContext.getRealPath("/resources/images/products/");
-		
+
 		File destination = new File(uploadPath); // 웹 서버 폴더
-		//String str = "/Users/ol7roeo/Documents/tempUpload"; // 가영 임시 폴더
-		String str = "C:/tempUpload";// 지혜 임시 폴더
+		String osName = System.getProperty("os.name").toLowerCase();
+		System.out.println("MyBean OS name : " + osName);
+		String str = "";
+		if (osName.contains("win")) 
+		{
+			System.out.println("사용자 OS - Window ");
+			str = "C:/tempUpload";
+		} 
+		else if (osName.contains("mac")) 
+		{
+			System.out.println("사용자 OS - MAC ");
+			str = "/Users/ol7roeo/Documents/tempUpload"; 
+		} 
 		File destination_local = new File(str); // 임시 폴더
-		
+
 		FileUtils.copyDirectory(destination_local, destination);
 	}
 
